023d13e04a9fb849a25fd385312db2b3e7574d66
Ingrid Silva <ingridsilvatec@outlook.com>
2025-01-29 04:38:01 -0300

    add layouts

---
+++ app/src/main/java/br/com/jpegsinng/layouts/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.jpegsinng.layouts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ex_scroll_view)
+    }
+}
